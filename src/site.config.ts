f3b6059aaf9e1385c19ff6dbc85dfc2d3c44d000
Oscar Sharpe <oscarsharpe@proton.me>
2023-02-14 01:40:48 +0000

    Initial Commit

---
+++ src/site.config.ts
@@ -0,0 +1,12 @@
+const siteMeta = {
+	author: "Oscar Sharpe",
+	title: "Oscar Sharpe",
+	description: "An opinionated starter theme for Astro",
+	lang: "en-GB",
+	ogLocale: "en_GB",
+	githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
+	themeColorLight: "#fafafa",
+	themeColorDark: "#1d1f21",
+};
+
+export default siteMeta;
